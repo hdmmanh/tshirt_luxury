fe27f0de1478ce539622b6cd23bf4ed5c1c60db8
hdmmanh <hdmmanh26@gmail.com>
2024-09-10 13:04:08 +0700

    push

---
+++ src/main/java/com/example/tshirt_luxury/TshirtLuxuryApplication.java
@@ -0,0 +1,13 @@
+package com.example.tshirt_luxury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtLuxuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtLuxuryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tshirt_luxury/TshirtLuxuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tshirt_luxury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshirtLuxuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
